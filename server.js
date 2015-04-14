1778b028378d73c00afcee343a2cc23d3f7b246e
Bert Austin Sherron <mail@austinsherron.com>
2015-04-13 17:13:42 -0700

    test

--- server.js
+++ server.js
@@ -4,14 +4,3 @@ var port = process.env.PORT || 3000;
 
 app.use('/', express.static(__dirname + '/public'));
 app.listen(port, function() { console.log('listening')});
-
-//var http = require('http');
-//
-//var server = http.createServer(function(req, res) {
-//	res.writeHead(200, {'Content-Type': 'text/html'});
-//	res.end('<h1>Hello World</h1>');
-//});
-//
-//var port = Number(process.env.PORT || 3000);
-//
-//server.listen(port);
