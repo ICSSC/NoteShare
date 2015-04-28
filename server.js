f99ad823dd11729f985532f74eb9f04fd5d64ce8
Yuhao Ma <yuhao93@gmail.com>
2015-04-27 17:43:31 -0700

    Changed from mustache to handlebars

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var mustache = require('mustache.js');
+var mustache = require('mustache');
 var port = process.env.PORT || 3000;
 
 app.use('/', express.static(__dirname + '/public'));
